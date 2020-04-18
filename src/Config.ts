012444dd5e09d6d5d6dc3fe4262eb50d2e659a3f
infinitycoding222 <58121837+infinitycoding222@users.noreply.github.com>
2020-04-18 16:33:21 +0300

    Update Config.ts

--- src/Config.ts
+++ src/Config.ts
@@ -1,4 +1,4 @@
 export let token: string = "NjYxNzM1MTM1Njk1MDc3Mzg2.XjIFcA.7YELgkgWzuIippqVubT8oFLkzUg"; // This is our bot token
 export let ownersid: string[] = ["473276250815856650", "540219416726601739"]; // this is our array of bot owners
 export let defprefix: string = ":?"; // this is our bot prefix
-export let databaseName: string = "a007db"
+export let databaseName: string = "a007db";
